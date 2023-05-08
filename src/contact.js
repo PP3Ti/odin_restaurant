a4fd260a6e860e9f52fa48178de21d48b2c0003e
PP3Ti <porkolab.peti@gmail.com>
2023-05-08 18:20:23 +0200

    image loading bugfix

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,3 @@
-import locationMap from './map.jpg'
 function createContacts() {
     const container = document.getElementById('content')
     container.replaceChildren()  // clear dom
@@ -15,7 +14,7 @@ function createContacts() {
     const address = document.createElement('p')
     address.textContent = '221B Baker Street, London'
     const map = document.createElement('img')
-    map.src = locationMap
+    map.src = './map.jpg'
     map.alt = 'map of location'
 
     contacts.append(getIn, tel, email, address, map)
